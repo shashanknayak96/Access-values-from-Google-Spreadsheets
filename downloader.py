a03d6bc242ef02d6d479d50b03defa0f787515f5
Shashank <shashank96.nayak@gmail.com>
2018-04-12 20:23:41 +0530

    second commit

---
+++ downloader.py
@@ -0,0 +1,15 @@
+import requests
+import csv 
+import os
+
+
+# os.chdir("C:/Users/Shashank/Desktop/MapTest")
+mydir = (os.path.dirname(os.path.realpath(__file__)))
+os.chdir(mydir)
+url = "<LINK TO THE PUBLISHED CSV FILE>"
+
+r = requests.get(url, allow_redirects=True)
+open('LatLngData.csv', 'wb').write(r.content)
+
+print("Completed downloading the file...")
+input("Press any key to close the window!")
